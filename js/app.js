dc7b9118da9424849f3a64410cbf410e752ba380
giusscos <giu.cosenza08@gmail.com>
2022-08-03 15:13:30 +0000

    Add Milestone 0

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+const team = [
+    {
+        'nome': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'image': 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        'nome': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'image': 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        'nome': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'walter-gordon-office-manager.jpg',
+    },
+    {
+        'nome': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        'nome': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'scott-estrada-developer.jpg',
+    },
+    {
+        'nome': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara-ramos-graphic-designer.jpg',
+    },
+]
+
+// console.log(team)
