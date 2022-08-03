6fbcc16bdac5b0a65b82faa00ef1ca53aa6822ad
giusscos <giu.cosenza08@gmail.com>
2022-08-03 16:38:08 +0000

    Bonus 3 Refactoring

--- Bonus-3/js/app.js
+++ Bonus-3/js/app.js
@@ -45,30 +45,42 @@ for(let key in team){
     const value = team[key];
     // console.log('Name >> ' + value.nome + '; Role >> ' + value.role + '; Image >> ' + value.image + ';');
     // divEl.innerHTML += ` Name: ${value.nome}; Role: ${value.role}; Image: <img src="./img/${value.image}"> <br>`;
-    const card = createCard(`./img/${value.image}`, value.nome, value.role);
+    const card = createCard(`./img/${value.image}`, `Immagine Profilo di ${value.nome}`, value.nome, value.role);
     gridEl.append(card);
 }
 
-function createCard(srcImage, nameProfile, roleProfile){
+function createCard(srcImage, altImage, nameProfile, roleProfile){
     const cardEl = document.createElement('div');
     cardEl.classList.add('card');
-    const infoEl = document.createElement('div');
-    infoEl.classList.add('info_profile', 'col');
 
+    const image = createImageProfile(srcImage, altImage);
+    const info = createInfoProfile(nameProfile, roleProfile);
+
+    cardEl.append(image, info);
+    return cardEl
+}
+
+function createImageProfile(srcImage, altProfile){
     const imageEl = document.createElement('img');
     imageEl.classList.add('image_profile');
     imageEl.src = srcImage;
+    imageEl.alt = altProfile;
+
+    return imageEl;
+}
+
+function createInfoProfile(name, role){
+    const infoEl = document.createElement('div');
+    infoEl.classList.add('info_profile', 'col');
 
     const nameEl = document.createElement('span');
     nameEl.classList.add('name_profile');
-    nameEl.innerHTML = nameProfile;
+    nameEl.innerHTML = name;
     const roleEl = document.createElement('span');
     roleEl.classList.add('role_profile');
-    roleEl.innerHTML = roleProfile;
+    roleEl.innerHTML = role;
 
     infoEl.append(nameEl);
     infoEl.append(roleEl);
-
-    cardEl.append(imageEl, infoEl);
-    return cardEl
+    return infoEl;
 }
